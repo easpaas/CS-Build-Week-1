ee4b48436e3073eaa1dbb4686d32ad8f2d3156b4
evan <aspaasevan@gmail.com>
2020-06-25 11:21:31 -0700

    refactor home page styles

--- react/game_of_life/src/components/Home.js
+++ react/game_of_life/src/components/Home.js
@@ -12,28 +12,31 @@ function Home () {
           <h2>Welcome to John Conway's "Game of Life"!</h2>
           <p>
             This is a computer science classic from 1970, a program that simulates a _cellular automaton_
-            (plural _automata_). It has connections to all kinds of different aspects of computer science and nature.
+            (plural _automata_). 
+          </p>
+          <p>
+            It has connections to all kinds of different aspects of computer science and nature.
           </p>
       </div>
 
       <div className="game-specs">
-        <h2>Rules of the game</h2>
-        <p>
-          In the Game of Life, these rules examine each cell of the grid. For each
-          cell, it counts that cell's eight neighbors (up, down, left, right, and
-          diagonals), and then act on that result.
-        </p>
-        <ul>
-          <li> 
+        <h2>Game Synopsis</h2>
+        <ol className='list'>
+          <li>
+            For each cell, it counts that cell's eight neighbors (up, down, left, right, and diagonals),and then act on that result.
+          </li>
+          <li>
             If the cell is alive AND has 2 or 3 neighbors, then it remains alive. Else it dies.
           </li>
           <li>
             If the cell is dead AND has exactly 3 neighbors then it comes to life.
           </li>
-        </ul>
+        </ol>
       </div> 
 
-      <Link to="/game"><button id="btn1">Get Started!</button></Link>
+      <Link to="/game">
+        <button id="btn1">Get Started!</button>
+      </Link>
     
     </div>
   );
